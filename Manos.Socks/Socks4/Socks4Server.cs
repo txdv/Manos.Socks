2bd38212ec610613ff7467b593602adddb2490cb
Andrius Bentkus <andrius.bentkus@gmail.com>
2011-07-06 11:45:39 +0200

    Fix race condition bug.
    The destination socket was not ready yet
    when the soruce socket could already write stuff.
    Buffer it and empty the buffer when connection established.

--- Manos.Socks/Socks4/Socks4Server.cs
+++ Manos.Socks/Socks4/Socks4Server.cs
@@ -5,10 +5,23 @@ using Manos.IO;
 
 namespace Manos.Socks
 {
+	public class Socks4ServerInfo
+	{
+		public Socks4ServerInfo()
+		{
+			Overhead = new Queue<byte[]>();
+		}
+		
+		public bool FirstMessage { get; set; }
+		public Socket DestinationSocket { get; set; }
+		public Socket SourceSocket { get; set; }
+		public bool DestinationReady { get; set; }
+		public Queue<byte[]> Overhead { get; set; }
+	}
+	
 	public class Socks4Server
 	{
-		private Dictionary<int, bool> firstMessage = new Dictionary<int, bool>();
-		private Dictionary<int, Socket> sockets = new Dictionary<int, Socket>();
+		private Dictionary<int, Socks4ServerInfo> info = new Dictionary<int, Socks4ServerInfo>();
 		
 		private Context Context { get; set; }
 		private Socket Socket { get; set; }
@@ -27,10 +40,13 @@ namespace Manos.Socks
 			Socket.Listen(host, port, delegate (Socket sourceSocket) {
 				var sourceStream = sourceSocket.GetSocketStream();
 				var sourcePort = sourceSocket.Port;
-				firstMessage[sourcePort] = true;
+				
+				info[sourcePort] = new Socks4ServerInfo() { FirstMessage = true };
+				
 				sourceStream.Read(delegate (ByteBuffer buffer) {
 					OnData(sourceSocket, sourceStream, buffer, checkUser);
 				}, delegate (Exception exception) { }, delegate {
+					info.Remove(sourcePort);
 				});
 			});
 		}
@@ -39,7 +55,8 @@ namespace Manos.Socks
 		{
 			var sourcePort = sourceSocket.Port;
 			
-			if (firstMessage[sourcePort]) {
+			if (info[sourcePort].FirstMessage) {
+
 				var data = Socks4Packet.GetPacketInfo(buffer.Bytes, buffer.Position, buffer.Length);
 				
 				if (!VersionCheck || !data.ValidVersion) {
@@ -62,9 +79,15 @@ namespace Manos.Socks
 					}
 					
 					var destinationSocket = Context.CreateSocket();
-					sockets[sourcePort] = destinationSocket;
+					info[sourcePort].DestinationSocket = destinationSocket;
 					destinationSocket.Connect(data.IPAddress.ToString(), data.Port, delegate {
 						var destinationStream = destinationSocket.GetSocketStream();
+						info[sourcePort].DestinationReady = true;
+						
+						var queue = info[sourcePort].Overhead;
+						while (queue.Count > 0) {
+							destinationStream.Write(queue.Dequeue());
+						}
 						destinationStream.Read(delegate (ByteBuffer destinationReadBuffer) {
 							sourceStream.Write(destinationReadBuffer);
 						}, delegate (Exception exception) { }, delegate {
@@ -74,19 +97,25 @@ namespace Manos.Socks
 					
 					sourceStream.Write(Socks4Packet.CreatePacket(Socks4Packet.CMD_REPLY_REQUEST_GRANTED));
 					
-					firstMessage[sourcePort] = false;
+					info[sourcePort].FirstMessage = false;
 					break;
 				default:
 					sourceStream.Close();
-					firstMessage.Remove(sourcePort);
+					info.Remove(sourcePort);
 					break;
 				}
 				
 			} else {
-				if (sockets.ContainsKey(sourcePort)) {
-					var destinationStream = sockets[sourcePort].GetSocketStream();
-					if (destinationStream != null && destinationStream.CanWrite) {
-						destinationStream.Write(buffer);
+				if (info.ContainsKey(sourcePort)) {
+					var sockInfo = info[sourcePort];
+					if (!sockInfo.DestinationReady) {
+						byte[] bytes = new byte[buffer.Length];
+						for (int i = 0; i < buffer.Length; i++) {
+							bytes[i] = buffer.Bytes[buffer.Position + i];
+						}
+						sockInfo.Overhead.Enqueue(bytes);
+					} else {
+						sockInfo.DestinationSocket.GetSocketStream().Write(buffer);
 					}
 				}
 			}
